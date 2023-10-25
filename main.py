cbe8861cc78eb0a823f0e6cc75dffd1a39a02404
Jhonny <gonzodzen@mail.ru>
2023-10-25 22:35:01 +0300

    Bubble sort

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from random import shuffle
 
-def choise_sort(arr):
+def bubble_sort(arr):
     a=arr.copy()
     l=len(a)
     shuffle(a)
-    for i in range(l):
-        for j in range(i+1,l):
-            if a[j]<a[i]:
-                a[i],a[j]=a[j],a[i]
+    for i in range(1,l):
+        for j in range(l-i):
+            if a[j]>a[j+1]:
+                a[j],a[j+1]=a[j+1],a[j]
     return a
 
 
-print(choise_sort(list(range(101))))
+print(bubble_sort(list(range(101))))
