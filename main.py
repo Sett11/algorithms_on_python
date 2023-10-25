c8ab11352bfd6ed02d3512437000a6d00fbb1544
Jhonny <gonzodzen@mail.ru>
2023-10-25 21:00:58 +0300

    Sieve of Eratosthenes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def primes(n):
-    if n<3:
+    if n<2:
         return 0
     a=[True]*(n+1)
     a[0]=a[1]=False
     for i in range(2,int(n**.5)+1):
-        if a[i]:
-            for j in range(i*i,n,i):
-                a[j]=False
+        for j in range(i*i,n,i):
+            a[j]=False
     return [k for k in range(2,n) if a[k]]
 
-print(primes(100000))
+print(primes(1000))
