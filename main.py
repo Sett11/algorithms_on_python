6471bfa3f6a3c358a8a53767ae234f162d9649ba
Jhonny <gonzodzen@mail.ru>
2023-10-25 23:20:11 +0300

    Dumb sort

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from random import shuffle
 
-def bubble_sort(arr):
-    a=arr.copy()
-    l=len(a)
-    shuffle(a)
-    for i in range(1,l):
-        for j in range(l-i):
-            if a[j]>a[j+1]:
-                a[j],a[j+1]=a[j+1],a[j]
-    return a
+def min_elem(a):
+    n=float('inf')
+    for i in range(len(a)):
+        n=n if n<a[i] else a[i]
+    return n
+
+def ind_min_elem(a,n):
+    return next(i for i in range(len(a)) if a[i]==n)
+
+def del_min_elem(a,n):
+    for i in range(len(a)):
+        if a[i]==n:
+            del a[i]
+            return a
 
 
-print(bubble_sort(list(range(101))))
+def dumb_sort(a):
+    r=[]
+    while len(a)>1:
+        j=ind_min_elem(a,min_elem(a))
+        r.append(a[j])
+        a=del_min_elem(a,a[j])
+    return r+a
+
+
+q = list(range(101))
+shuffle(q)
+print(dumb_sort(q))
