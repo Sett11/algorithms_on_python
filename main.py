025b6c9df0bfe2bda2fe4cc98112491a3f905bcf
Jhonny <gonzodzen@mail.ru>
2023-10-26 00:08:26 +0300

    Greatest common divisor (gsd)

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from random import shuffle
-
-def min_elem(a):
-    n=float('inf')
-    for i in range(len(a)):
-        n=n if n<a[i] else a[i]
-    return n
-
-def ind_min_elem(a,n):
-    return next(i for i in range(len(a)) if a[i]==n)
-
-def del_min_elem(a,n):
-    for i in range(len(a)):
-        if a[i]==n:
-            del a[i]
-            return a
+def gcd(a,b):
+    return a if not b else gcd(b,a%b)
 
 
-def dumb_sort(a):
-    r=[]
-    while len(a)>1:
-        j=ind_min_elem(a,min_elem(a))
-        r.append(a[j])
-        a=del_min_elem(a,a[j])
-    return r+a
-
-
-q = list(range(101))
-shuffle(q)
-print(dumb_sort(q))
+print(gcd(70,49))
+print(gcd(49,70))
