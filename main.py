eef4e5b369a90ae6fe22c8d63ef74a3ff7a01131
Jhonny <gonzodzen@mail.ru>
2023-10-27 12:21:34 +0300

    Longest common subsequence (length)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-def count_way(a):
-    m,n=len(a),len(a[0])
-    a[0]=[1]*n
-    for i in range(1,m):
-        a[i][0]=1
-    for i in range(1,m):
-        for j in range(1,n):
-            a[i][j]=a[i-1][j]+a[i][j-1]+a[i-1][j-1]
-    return a
+def longest_common_subsequence(a,b):
+    n,m=len(a),len(b)
+    r=[[0]*(m+1) for i in range(n+1)]
+    s=0
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if a[i-1]==b[j-1]:
+                r[i][j]=1+r[i-1][j-1]
+            else:
+                r[i][j]=max(r[i-1][j],r[i][j-1])
+            s=max(s,r[i][j])
+    return s
 
-
-print(count_way([[0 for i in range(7)] for j in range(7)]))
+print(longest_common_subsequence('abcde','ace'))
+print(longest_common_subsequence([1,2,3,4,4,4,4,5,6,7],[0,3,4,4,4,4,4,7,6]))
