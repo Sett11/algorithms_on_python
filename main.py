f55fe9af7868c92c19970602b96052be2c9684ae
Jhonny <gonzodzen@mail.ru>
2023-10-27 17:02:58 +0300

    Levenshtein distance

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-def largest_increacing_subcequence(a):
-    n=len(a)
-    r=[0]*(n+1)
+def levenshtein(a,b):
+    n,m=len(a),len(b)
+    r=[[i+j if i*j==0 else 0 for j in range(m+1)] for i in range(n+1)]
     for i in range(1,n+1):
-        m=0
-        for j in range(i):
-            if a[i-1]>a[j-1] and r[j]>m:
-                m=r[j]
-        r[i]=m+1
-    return r[-1]
+        for j in range(1,m+1):
+            if a[i-1]==b[j-1]:
+                r[i][j]=r[i-1][j-1]
+            else:
+                r[i][j]=1+min(r[i-1][j],r[i][j-1],r[i-1][j-1])
+    return r[-1][-1]
 
-print(largest_increacing_subcequence([1,2,3,4,4,4,4,5,6,7]))
+
+print(levenshtein('nayvyedosf', 'sjxen'))
