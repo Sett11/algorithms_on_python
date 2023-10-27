2f00f3aac1282624274e73599e0f5e3db1c327d5
Jhonny <gonzodzen@mail.ru>
2023-10-27 09:50:09 +0300

    Binary_search (in sorted array)

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-def prod(*a):
-    return {()} if not a else [tuple([i]+list(j)) for i in a[0] for j in prod(*a[1:])]
-    
+def left(a,n):
+    l=1
+    r=len(a)
+    while r-l>1:
+        m=(l+r)//2
+        if a[m]<n:
+            l=m
+        else:
+            r=m
+    return l+1
 
-print(prod({1},{2,3},{4,5,6},{7,8,9}))
+def right(a,n):
+    l=1
+    r=len(a)
+    while r-l>1:
+        m=(l+r)//2
+        if a[m]<=n:
+            l=m
+        else:
+            r=m
+    return r
+
+def binary_search(a,n):
+    return n in a[left(a,n):right(a,n)]
+
+print(binary_search([0, 1, 1, 2, 3, 3, 4, 4, 6, 59],3))
