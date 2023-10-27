9f74df73bb0e66803ffe2e4c9de1e6a99ca5fdb6
Jhonny <gonzodzen@mail.ru>
2023-10-27 12:46:22 +0300

    Largest increacing subcequence

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def longest_common_subsequence(a,b):
-    n,m=len(a),len(b)
-    r=[[0]*(m+1) for i in range(n+1)]
-    s=0
+def largest_increacing_subcequence(a):
+    n=len(a)
+    r=[0]*(n+1)
     for i in range(1,n+1):
-        for j in range(1,m+1):
-            if a[i-1]==b[j-1]:
-                r[i][j]=1+r[i-1][j-1]
-            else:
-                r[i][j]=max(r[i-1][j],r[i][j-1])
-            s=max(s,r[i][j])
-    return s
+        m=0
+        for j in range(i):
+            if a[i-1]>a[j-1] and r[j]>m:
+                m=r[j]
+        r[i]=m+1
+    return r[-1]
 
-print(longest_common_subsequence('abcde','ace'))
-print(longest_common_subsequence([1,2,3,4,4,4,4,5,6,7],[0,3,4,4,4,4,4,7,6]))
+print(largest_increacing_subcequence([1,2,3,4,4,4,4,5,6,7]))
