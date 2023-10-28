5629c7c0ae12c4adb891fde9c0c4b282c96915cc
Jhonny <gonzodzen@mail.ru>
2023-10-28 15:16:57 +0300

    Diagonals

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-def levenshtein(a,b):
-    n,m=len(a),len(b)
-    r=[[i+j if i*j==0 else 0 for j in range(m+1)] for i in range(n+1)]
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-            if a[i-1]==b[j-1]:
-                r[i][j]=r[i-1][j-1]
+def count_way(a):
+    n,m=len(a),len(a[0])
+    a[0]=[1]*m
+    for i in range(n):
+        a[i][0]=1
+    for i in range(1,n):
+        for j in range(1,m):
+            a[i][j]=a[i-1][j]+a[i][j-1]
+    return a
+
+def diagonals(a):
+    r=[]
+    q=[]
+    v=2
+    while v:
+        for i in range(len(a)-(0 if v==2 else 1)):
+            k=i
+            j=0
+            t=[]
+            while k>=0 and j<len(a[0]):
+                t.append(a[j][k])
+                j+=1
+                k-=1
+            if v==2:
+                r.append(t)
             else:
-                r[i][j]=1+min(r[i-1][j],r[i][j-1],r[i-1][j-1])
-    return r[-1][-1]
+                q.insert(0,t)
+        v-=1
+        a=[p[::-1] for p in a][::-1]
+    return r+q
 
-
-print(levenshtein('nayvyedosf', 'sjxen'))
+print(diagonals(count_way([[0]*5 for _ in range(5)])))
