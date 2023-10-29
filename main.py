c87f141ee777e9833ade3a701d349d8f6de9278f
Jhonny <gonzodzen@mail.ru>
2023-10-29 19:17:00 +0300

    Testing

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-def count_way(a):
-    n,m=len(a),len(a[0])
-    a[0]=[1]*m
-    for i in range(n):
-        a[i][0]=1
-    for i in range(1,n):
-        for j in range(1,m):
-            a[i][j]=a[i-1][j]+a[i][j-1]
-    return a
+"""
+>>> is_empty()
+True
+>>> push(1)
+>>> push(2)
+>>> push(3)
+>>> is_empty()
+False
+>>> pop()
+3
+>>> pop()
+2
+>>> pop()
+1
+>>> is_empty()
+True
+"""
+_stack=[]
 
-def diagonals(a):
-    r=[]
-    q=[]
-    v=2
-    while v:
-        for i in range(len(a)-(0 if v==2 else 1)):
-            k=i
-            j=0
-            t=[]
-            while k>=0 and j<len(a[0]):
-                t.append(a[j][k])
-                j+=1
-                k-=1
-            if v==2:
-                r.append(t)
-            else:
-                q.insert(0,t)
-        v-=1
-        a=[p[::-1] for p in a][::-1]
-    return r+q
+def is_empty():
+    return not len(_stack)
 
-print(diagonals(count_way([[0]*5 for _ in range(5)])))
+def push(n):
+    _stack.append(n)
+
+def pop():
+    return _stack.pop()
+
+def clear():
+    _stack.clear()
+
+if __name__=="__main__":
+    from doctest import testmod
+    testmod(verbose=True)
