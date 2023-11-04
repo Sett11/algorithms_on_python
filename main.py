4e738ea46484b19ae33bdd03cf5fc0a4a738fdbc
Jhonny <gonzodzen@mail.ru>
2023-11-04 15:29:29 +0300

    longest_increasing_path (DFS)

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-def convert_to_base(n,b):
-    s='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    if b>len(s):
-        raise()
-    r=''
-    while n:
-        r=s[n%b]+r
-        n//=b
-    return r
+def longestIncreasingPath(a):
+    n,m=len(a),len(a[0])
+    r=[]
 
-print(convert_to_base(1221,16))
+    def dfs(i,j,w,c):
+        if i<0 or i>=n or j<0 or j>=m or a[i][j]<=w:
+            r.append(c)
+            return
+        dfs(i+1,j,a[i][j],c+1)
+        dfs(i-1,j,a[i][j],c+1)
+        dfs(i,j+1,a[i][j],c+1)
+        dfs(i,j-1,a[i][j],c+1)
+
+    for i in range(n):
+        for j in range(m):
+            dfs(i,j,-1,0)
+            if n in a:
+                break
+
+    return max(r)
+
+
+print(longestIncreasingPath([[3,4,5],
+           [3,2,6],
+           [2,2,1]]))
