0c85683b07b3838417822e09c0d41a00c7a666c8
Jhonny <gonzodzen@mail.ru>
2023-11-15 20:06:57 +0300

    BFS in graph (chess bishop)

--- gen_graph.py
+++ gen_graph.py
@@ -1,4 +1,4 @@
-def generate_chess_grafh():
+def generate_chess_knight_grafh():
     letters,numbers='abcdefgh','12345678'
     g={i+j:set() for i in letters for j in numbers}
     add_g=lambda e,c:g[e].add(c) and g[c].add(e)
@@ -19,3 +19,28 @@ def generate_chess_grafh():
                 v2=letters[i-1]+numbers[j+2]
                 add_g(v1,v2)
     return g
+
+def generate_chess_bishop_graph():
+    letters='abcdefgh'
+    numbers='12345678'
+    g={i+j:set() for i in letters for j in numbers}
+    add=lambda e,c:g[e].add(c) and g[c].add(e)
+
+    for i in range(8):
+        for j in range(8):
+            v1=letters[i]+numbers[j]
+            t=[[k,t] for k,t in [[i+1,j+1],[i-1,j-1],[i-1,j+1],[i+1,j-1]] if 0<=k<8 and 0<=t<8]
+            for e,u in t:
+                add(v1,letters[e]+numbers[u])
+    
+    return g
+
+d_graph={'A':{'B':2,'H':15},
+   'B':{'C':1,'D':5},
+   'C':{'B':1,'D':3,'G':1,'F':2},
+   'D':{'C':3,'F':4,'E':6},
+   'G':{'C':1,'F':1},
+   'F':{'C':2,'G':1,'D':4,'E':7,'H':3},
+   'E':{'F':7,'D':6,'I':2},
+   'I':{'E':2,'H':12},
+   'H':{'A':15,'I':12}}
--- main.py
+++ main.py
@@ -1,43 +1,30 @@
 from collections import deque
+from gen_graph import generate_chess_bishop_graph
 
-g={'A':{'B':2,'H':15},
-   'B':{'C':1,'D':5},
-   'C':{'B':1,'D':3,'G':1,'F':2},
-   'D':{'C':3,'F':4,'E':6},
-   'G':{'C':1,'F':1},
-   'F':{'C':2,'G':1,'D':4,'E':7,'H':3},
-   'E':{'F':7,'D':6,'I':2},
-   'I':{'E':2,'H':12},
-   'H':{'A':15,'I':12}}
 
-def dijcstra(s):
-    q=deque([s])
-    w={s:0}
+def bst(x,y):
+    g=generate_chess_bishop_graph()
+    distances={i:None for i in g}
+    parents=distances.copy()
+    distances[x]=0
+    q=deque([x])
 
     while q:
         v=q.popleft()
         for i in g[v]:
-            t=w[v]+g[v][i]
-            if i not in w or t<w[i]:
-                w[i]=t
-                q.appendleft(i)
+            if distances[i] is None:
+                distances[i]=distances[v]+1
+                parents[i]=v
+                q.append(i)
     
-    return w
+    path=[y]
+    parent=parents[y]
 
-def cheap_path_in_graph(s,e):
-    m=dijcstra(s)
-    r=[e]
+    while not parent is None:
+        path.append(parent)
+        parent=parents[parent]
 
-    while r[-1]!=s:
-        t=r[-1]
-        n=m[t]
-        p=[i for i in g[t] if n-g[t][i]==m[i]]
-        if not p:
-            break
-        next=p[0]
-        r.append(next)
-        
-    r.append(s)
-    return r[::-1]
+    return path[::-1]
 
-print(cheap_path_in_graph('A','I'))
+
+print(bst('a1','e3'))
