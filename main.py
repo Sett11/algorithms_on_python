c6d1d7c7393bd2fe0ab46f4b84b2de6178b5dc8e
Jhonny <gonzodzen@mail.ru>
2023-11-23 20:33:14 +0300

    Merge_sort

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-from math import ceil
+from random import shuffle
 
-def eratosthenus(n):
+def merge(a,b):
+    n,m=len(a),len(b)
+    r=[0]*(n+m)
+    i=j=k=0
+
+    while i<n and j<m:
+        if a[i]<=b[j]:
+            r[k]=a[i]
+            i+=1
+        else:
+            r[k]=b[j]
+            j+=1
+        k+=1
+    
+    while i<n:
+        r[k]=a[i]
+        i+=1
+        k+=1
+
+    while j<m:
+        r[k]=b[j]
+        j+=1
+        k+=1
+
+    return r
+
+
+def merge_sort(a):
+    n=len(a)
     if n<2:
-        return []
-    r=[True]*(n+1)
-    r[0]=r[1]=False
+        return
+    m=n//2
+    l=[a[i] for i in range(m)]
+    r=[a[i] for i in range(m,n)]
+    merge_sort(l)
+    merge_sort(r)
+    c=merge(l,r)
 
-    for i in range(2,int(n**.5+1)):
-        if r[i]:
-            r[2*i:n+1:i]=[False]*ceil(((n+1)/i)-2)
+    for i in range(len(c)):
+        a[i]=c[i]
 
-    return [i for i,j in enumerate(r) if j]
+    return a
+
+q=list(range(1002))
+shuffle(q)
 
 
-print(eratosthenus(10001))
+print(merge_sort(q))
