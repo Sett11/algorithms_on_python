d762d5ea738f66196addf13a2a62d5c163edd28d
Jhonny <gonzodzen@mail.ru>
2023-12-20 12:12:05 +0300

    All sums array...

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from collections import defaultdict
+from random import shuffle,choice#,randint
+from re import sub
+from math import ceil,inf
+from collections import defaultdict,deque
+from string import ascii_uppercase
+from gen_graph import p_graph as g
 
-def longest_arithSeq_length(self,a):
-    o=defaultdict(dict)
-    c=1
 
-    for i in range(len(a)):
-        for j in range(i):
-            n=a[i]-a[j]
-            if n not in o[j]:
-                o[j][n]=1
-            if n not in o[i]:
-                o[i][n]=0
-            o[i][n]=o[j][n]+1
-            c=max(c,o[i][n])
+from random import randint
 
-    return c
+def f(a,s):
+    n,r=len(a),set()
+    for i in range(1,2**n+1):
+        t,p=[],1
+        for j in range(n):
+            if i&p:
+                t.append(a[j])
+                if sum(t)>s:
+                    break
+            p*=2
+        if sum(t)==s:
+            r.add(tuple(sorted(t)))
+    return r
 
-print(longest_arithSeq_length([9,4,7,2,10]))
+
+print(f(sorted(randint(1,101) for i in range(20)),78))
