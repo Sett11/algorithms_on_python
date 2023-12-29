f9813b520a2f1b64fa168bc2a1228e23b19c6686
Jhonny <gonzodzen@mail.ru>
2023-12-29 14:34:07 +0300

    All combinations number

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ from string import ascii_uppercase
 from gen_graph import matrix
 
 
-def mp(a,x,y):
-    if [x,y]==[0,0]:
-        return a[0][0]
-    n,m=x+1,y+1
-    r=[[0]*m for _ in range(n)]
-    r[0][0]=a[0][0]
-    for i in range(n):
-        r[i][0]=r[i-1][0]+a[i][0]
-    for i in range(m):
-        r[0][i]=r[0][i-1]+a[0][i]
-    for i in range(1,n):
-        for j in range(1,m):
-            r[i][j]=min(r[i-1][j],r[i][j-1])+a[i][j]
-    return r[-1][-1]
+def f(a,j,n,r,q=[]):
+    if(r<0):
+        return
+    if(r==0):
+        t=[]
+        for i in range(j):
+            t.append(a[i])
+        q.append(t)
+        return
+    p=1 if j==0 else a[j-1]
+    for k in range(p,n+1):
+        a[j]=k
+        f(a,j+1,n,r-k,q)
+    return q
 
-print(mp(matrix,6,6))
+def combos(n):
+    return f([0]*n,0,n,n,[])
+
+print(combos(13))
