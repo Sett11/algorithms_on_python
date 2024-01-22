c391d51b7926909f2a4001c2ac79601c80995b75
Jhonny <gonzodzen@mail.ru>
2024-01-23 01:03:53 +0300

    Depth list

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
 from random import shuffle,choice#,randint
 from re import sub
-from math import ceil,inf
-from collections import defaultdict,deque
+from math import ceil, inf
+from collections import defaultdict, deque
 from string import ascii_uppercase
-from gen_graph import matrix
+from gen_graph import matrix, dfs_a, dfs_b, generate_chess_knight_grafh, p_graph as g, d_graph as dg
 
 
-def f(a,j,n,r,q=[]):
-    if(r<0):
-        return
-    if(r==0):
-        t=[]
-        for i in range(j):
-            t.append(a[i])
-        q.append(t)
-        return
-    p=1 if j==0 else a[j-1]
-    for k in range(p,n+1):
-        a[j]=k
-        f(a,j+1,n,r-k,q)
-    return q
+def depth_list(a):
+    return 1+max(depth_list(i) for i in a) if a and isinstance(a,list) else 0
 
-def combos(n):
-    return f([0]*n,0,n,n,[])
-
-print(combos(13))
+print(depth_list([[[[]]]]))
