df678a2601be504c66a15275ea7be4f30e37b341
Jhonny <gonzodzen@mail.ru>
2024-01-30 11:44:46 +0300

    New algoritm all sum nuber

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from collections import defaultdict, deque
 from string import ascii_uppercase
 from gen_graph import matrix, dfs_a, dfs_b, generate_chess_knight_grafh, p_graph as g, d_graph as dg
 
+def f(a,j,r,q):
+    if not r:
+        t=[]
+        for i in range(j):
+            t.append(a[i])
+        q.append(t)
+        return
+    p=1 if j==0 else a[j-1]
+    for i in range(p,r+1):
+        a[j]=i
+        f(a,j+1,r-i,q)
+    return q
 
-def depth_list(a):
-    return 1+max(depth_list(i) for i in a) if a and isinstance(a,list) else 0
+def comb(n):
+    return f([0]*n,0,n,[])
 
-print(depth_list([[[[]]]]))
+print(comb(22))
