b3dd88340cff0387c17d4a43a33f6c73ab390bed
Jhonny <gonzodzen@mail.ru>
2024-06-07 10:21:42 +0300

    Points in a Gaussian circle

--- main.py
+++ main.py
@@ -6,16 +6,8 @@ from string import ascii_uppercase,digits
 from gen_graph import matrix, dfs_a, dfs_b, seq, generate_chess_knight_grafh, p_graph as g, d_graph as dg
 from pprint import pprint
 
-def find_longest_substring_bracket(s):
-    stask,m=[-1],0
-    for i,j in enumerate(s):
-        if j=='(':
-            stask.append(i)
-        else:
-            stask.pop()
-            if stask:m=max(m,i-stask[-1])
-            else:
-                stask.append(i)
-    return m
+def points(n):
+    return 1+4*sum((n*n-i*i)**.5//1 for i in range(n))
 
-pprint(find_longest_substring_bracket(')(())()()'),width=50,indent=3)
+
+print(points(1000))
