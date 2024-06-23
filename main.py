b4c6881b5358ba75dd50675a59a287753e8c5da5
Jhonny <gonzodzen@mail.ru>
2024-06-23 10:09:34 +0300

    Integer_partition

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from string import ascii_uppercase,digits
 from gen_graph import matrix, dfs_a, dfs_b, seq, generate_chess_knight_grafh, p_graph as g, d_graph as dg
 from pprint import pprint
 
-def points(n):
-    return 1+4*sum((n*n-i*i)**.5//1 for i in range(n))
 
+def integer_partition(n):
+    d=[1]+[0]*n
+    for i in range(1,n+1):
+        for j in range(i,n+1):
+            d[j]+=d[j-i]
+    return d[-1]
 
-print(points(1000))
+print(integer_partition(100))
