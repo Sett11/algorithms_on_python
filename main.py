a2c1c9773845468aba0ac89988873b3cfd924c57
Jhonny <gonzodzen@mail.ru>
2024-07-05 14:57:10 +0300

    Pisano period mod 10

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from re import sub
 from math import ceil, inf
 from collections import defaultdict, deque, Counter
-from string import ascii_uppercase,digits
+from string import ascii_uppercase, digits
 from gen_graph import matrix, dfs_a, dfs_b, seq, seq_b, generate_chess_knight_grafh, p_graph as g, d_graph as dg, f_grapf as fg, ff_g
-from pprint import pprint
-from functools import lru_cache,reduce
+from functools import reduce
 from itertools import product
 from operator import mul
 from random import shuffle,choice#,randint
+q=list(range(1,1503))
+shuffle(q)
 
 
-def range_sum(a,b):
-    return (a+b)*(b-a+1)//2
+def pisano_mod_10(n):
+    n%=60
+    a,b=0,1
+    for _ in range(n):
+        a,b=b,a+b
+    return (a*b)%10
 
-print(range_sum(666,1090987780))
+print(pisano_mod_10(10000000))
