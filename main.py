694b5de5a3906d3962d79fa2a71cae0fe6c4c7dd
Jhonny <gonzodzen@mail.ru>
2024-07-24 14:08:54 +0300

    Syntax parse tree

--- main.py
+++ main.py
@@ -5,24 +5,40 @@ from string import ascii_uppercase, digits
 from gen_graph import matrix, dfs_a, dfs_b, seq, seq_b, generate_chess_knight_grafh, p_graph as g, d_graph as dg, f_grapf as fg, ff_g
 from functools import reduce
 from itertools import product
-from operator import mul
+from operator import mul,add,sub,truediv
 from random import shuffle,choice#,randint
 q=list(range(1,1503))
 shuffle(q)
 
 
-def comb(a,r):
-    p,n,t,res=tuple(a),len(a),list(range(r)),[]
-    f=lambda:res.append(tuple(p[i] for i in t))
-    while 1:
-        f()
-        for i in reversed(range(r)):
-            if t[i]!=i+n-r:
-                break
-        else:
-            return res
-        t[i]+=1
-        for j in range(i+1,r):
-            t[j]=t[j-1]+1
+class Node:
+    def __init__(self,data=None,parent=None,left=None,right=None):
+        self.data=data
+        self.parent=parent
+        self.left=left
+        self.right=right
 
-print(comb([1,2,3,4,5],3))
+
+def f(s):
+    h=c=Node()
+    for i in s:
+        if i=='(':
+            c.left=Node(parent=c)
+            c=c.left
+        elif i==')':
+            c=c.parent
+        elif i in '+-*/':
+            c.data=i
+            c.right=Node(parent=c)
+            c=c.right
+        else:
+            c.data=int(i)
+            c=c.parent
+    return h
+
+def ev(h):
+    ops={'-':sub,'+':add,'*':mul,'/':truediv}
+    l,r=h.left,h.right
+    return ops[h.data](ev(l),ev(r)) if l and r else h.data
+
+print(ev(f('((7+3)*(((5-2)+2)*3))')))
