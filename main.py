7e77cb620c3982eb7eeb2c17a51a1c2a8c9b10b6
Jhonny <gonzodzen@mail.ru>
2024-07-30 15:38:54 +0300

    Stack

--- main.py
+++ main.py
@@ -11,56 +11,26 @@ q=list(range(1,1503))
 shuffle(q)
 
 
-class BinHeap:
-    def __init__(self,heap_list=[0],size=0):
-        self.heap_list=heap_list
-        self.size=size
-
-    def pers_up(self,i):
-        while i//2:
-            if self.heap_list[i]<self.heap_list[i//2]:
-                self.heap_list[i],self.heap_list[i//2]=self.heap_list[i//2],self.heap_list[i]
-            i//=2
+class Stack:
+    def __init__(self,stack=[]):
+        self.stack=stack
+        self.size=len(stack)
     
-    def insert(self,v):
-        self.heap_list.append(v)
+    def push(self,v):
+        self.stack.append(v)
         self.size+=1
-        self.pers_up(self.size)
-
-    def pers_down(self,i):
-        while i*2<self.size:
-            m=self.min_child(i)
-            if self.heap_list[i]>self.heap_list[m]:
-                self.heap_list[i],self.heap_list[m]=self.heap_list[m],self.heap_list[i]
-            i=m
-
-    def min_child(self,i):
-        if i*2+1>self.size or self.heap_list[i*2]<self.heap_list[i*2+1]:
-            return i*2
-        return i*2+1
     
-    def del_min(self):
-        x=self.heap_list[1]
-        self.heap_list[1]=self.size-1
+    def pop(self):
         self.size-=1
-        self.heap_list.pop()
-        self.pers_down(1)
-        return x
+        return self.stack.pop()
     
-    def build_heap(self,a):
-        n=len(a)
-        i,self.size=n//2,n
-        self.heap_list=[0]+a[:]
-        while i:
-            self.pers_down(i)
-            i-=1
-
-h=BinHeap()
-h.build_heap([9,5,6,2,3])
-print(h.heap_list)
-print(h.del_min())
-print(h.size)
-h.insert(12)
-print(h.heap_list)
-h.insert(1)
-print(h.heap_list,h.size)        
+    def is_empty(self):
+        return not self.stack
+    
+    def peek(self):
+        return self.stack[-1] if not self.is_empty() else None
+    
+s=Stack()
+s.push(2)
+s.push(3)
+print(s.peek(),s.pop(),s.size)
