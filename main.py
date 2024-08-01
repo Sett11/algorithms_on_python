17755e437c7aa22fd51d94b68908cb75f7bca79b
Jhonny <gonzodzen@mail.ru>
2024-08-01 15:15:04 +0300

    Prefix polish notation

--- main.py
+++ main.py
@@ -11,59 +11,13 @@ q=list(range(1,1503))
 shuffle(q)
 
 
-class PrefixTree:
-    def __init__(self,key=None,parent=None):
-        self.key=key
-        self.parent=parent
-        self.children={}
-        self.end=False
-    
-    def get_word(self):
-        r,h=[],self
-        if h is not None:
-            r.insert(0,h.key)
-            h=h.parent
-        return ''.join(r)
-    
-    def contain(self,s):
-        h=self
-        for i in s:
-            if i in h.children:
-                h=h.children[i]
-            else:
-                return False
-        return h.end
-    
-    def insert(self,s):
-        h=self
-        for i in s:
-            if i not in h.children:
-                h.children[i]=PrefixTree(i,h)
-            h=h.children[i]
-        h.end=True
+def calculate(s):
+    stack=[]
+    for i in s.split()[::-1]:
+        if re.sub(r'[-.]','',i).isdigit():
+            stack.append(float(i))
+        else:
+            stack.append(eval(f'{stack.pop()}{i}{stack.pop()}'))
+    return stack[-1]
 
-    def remove(self,s):
-        h=self
-        def find_word(h,s,i):
-            if i==len(s):
-                if not h.end:
-                    return False
-                h.end=False
-                return len(h.children)==0
-            if find_word(h.children[s[i]],s,i+1):
-                del h.children[s[i]]
-                return not h.end and len(h.children)==0
-            return False
-        find_word(h,s,0)
-
-    def __repr__(self):
-        return str(self.children)
-
-p=PrefixTree()
-p.insert('Hell')
-p.insert('Hello')
-print(p.contain('Hello'),p.contain('Hell'))
-p.insert('World')
-p.remove('Hell')
-print(p.contain('Hell'))
-print(repr(p))
+print(calculate('/ + 3 5 * 2 2'))
