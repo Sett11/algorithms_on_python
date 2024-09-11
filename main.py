88c9dccec63604d87d30a053dc1ba70c67052061
Jhonny <gonzodzen@mail.ru>
2024-09-12 00:13:51 +0300

    Linear regression X by Y with numpy

--- main.py
+++ main.py
@@ -21,13 +21,10 @@ def fucken_indentations():
    ...
 
 
-# def regression_line_with_np(x,y):
-#     a,b=np.polyfit(x,y,1)
-#     return round(b,4),round(a,4)
 
-def regression_line(x,y):
-    n,xq,xs,ys,xys=len(x),sum(i**2 for i in x),sum(x),sum(y),sum(i*j for i,j in zip(x,y))
-    k=n*xq-xs**2
-    return round((xq*ys-xs*xys)/k,4),round((n*xys-xs*ys)/k,4)
 
-print(regression_line([56,42,72,36,63,47,55,49,38,42,68,60],[147,125,160,118,149,128,150,145,115,140,152,155]))
+def regression_line_with_np(x,y):
+    a,b=np.polyfit(x,y,1)
+    return round(b,4),round(a,4)
+
+print(regression_line_with_np([56,42,72,36,63,47,55,49,38,42,68,60],[147,125,160,118,149,128,150,145,115,140,152,155]))
