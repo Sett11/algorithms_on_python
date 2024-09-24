3e3faf587ff8875bfeee0b4a81248569281af0b7
Jhonny <gonzodzen@mail.ru>
2024-09-25 01:15:18 +0300

    Two`s complement and reverse operation

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-x=1000000
-a,b,c='012'*x,'000111222'*x,('0'*9+'1'*9+'2'*9)*x
+import numpy as np
+import gmpy2
+import inspect
+import bisect
+import networkx as nx
+import matplotlib.pyplot as plt
+import re
+from math import ceil,floor,inf,factorial,log10,pi,e
+from collections import defaultdict,deque,Counter
+from string import ascii_uppercase,digits
+from gen_graph import matrix,dfs_a,dfs_b,seq,seq_b,generate_chess_knight_grafh,p_graph as g,d_graph as dg,f_grapf as fg, ff_g,exp,exp_dict,net_g,options,back,sudoku,kad
+from functools import reduce
+from itertools import product,zip_longest,permutations
+from copy import deepcopy
+from operator import mul,add,sub,truediv,mod
+from decimal import Decimal
+from random import shuffle,choice#,randint
 
-def get_positions(n):
-    return int(a[n]),int(b[n]),int(c[n])
 
-print(get_positions(98))
+
+def twos_comp(n,l):
+    if (n&(1<<(l-1))):
+        n-=(1<<l)
+    return n
+
+def num_to_bin(n,l):
+    if n<0:
+        n=2**l+n
+    b=bin(n)[2:]
+    s=l-len(b)
+    return '0'*s+b
