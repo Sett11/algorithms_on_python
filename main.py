4c25b5586de8d022dccebd95ff8e3de1a981145e
Jhonny <gonzodzen@mail.ru>
2024-11-12 00:16:24 +0300

    LZ78 compression

--- main.py
+++ main.py
@@ -18,13 +18,31 @@ from random import shuffle,choice#,randint
 
 
 
-from re import split,findall
-from functools import reduce
+def encoder(s):
+    d,r,c,k={s[0]:'1'},'0'+s[0],2,''
+    for i in s[1:]:
+        k+=i
+        if k not in d:
+            d[k]=str(c)
+            if len(k)==1:
+                r+='0'+k
+            else:
+                r+=d[k[0:-1]]+k[-1]
+            c+=1
+            k=''
+    return r+d.get(k,'')
 
-def f(e,x):
-    k,p=split(r'x\^?',e)
-    k,p=-1 if k=='-' else 1 if k=='' else int(k),1 if p=='' else int(p)
-    return p*k*pow(x,p-1)
+def decoder(s):
+    d,r,c,k={'0':'','1':s[1]},s[1],'',2
+    for i in s[2:]:
+        if i.isdigit():
+            c+=i
+        else:
+            d[str(k)]=d[c]+i
+            r+=d[c]+i
+            c=''
+            k+=1
+    return r+d.get(c,'')
 
-def differentiate(e,p):
-    return reduce(lambda a,c:a+f(c,p),[i[0] for i in findall(r'(-?\d*x(\^\d+)?)',e)],0)
+print(encoder('ABAABABAABABAA'))
+print(decoder('0A0B1A2A4A4B3'))
